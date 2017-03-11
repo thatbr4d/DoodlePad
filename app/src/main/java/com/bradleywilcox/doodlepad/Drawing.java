5828e75759940e8a876c807155679dc560f1dfe7
Brad <thatbr4d@gmail.com>
2017-03-10 19:43:25 -0500

    drawing extended view and main layout

---
+++ app/src/main/java/com/bradleywilcox/doodlepad/Drawing.java
@@ -0,0 +1,120 @@
+package com.bradleywilcox.doodlepad;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Brad on 3/10/2017.
+ */
+
+public class Drawing extends View {
+
+    private int currentWidth;
+    private int currentHeight;
+
+    private Paint paint;
+    private float x;
+    private float y;
+
+    public Drawing(Context context) {
+        super(context);
+        setup(null);
+    }
+
+    public Drawing(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup(attrs);
+    }
+
+    public Drawing(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setup(attrs);
+    }
+
+    private void setup(AttributeSet attrs){
+        paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+
+        //update to dip later
+        int pixels = 50;
+        canvas.drawRect(x, y, x+pixels, y+pixels, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                y = touchY;
+                x = touchX;
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                y = touchY;
+                x = touchX;
+
+                break;
+            case MotionEvent.ACTION_UP:
+
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int desiredWidth = 100;
+        int desiredHeight = 100;
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            width = desiredWidth;
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            height = desiredHeight;
+        }
+
+        setMeasuredDimension(width, height);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        currentWidth = w;
+        currentHeight = h;
+    }
+}
