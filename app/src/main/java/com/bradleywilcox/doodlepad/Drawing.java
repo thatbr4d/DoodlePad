0e0a30853df35b41aa2c9e9c84ab22d550909c57
Brad <thatbr4d@gmail.com>
2017-03-13 17:23:52 -0400

    add line tool and allow switching between line and rect

--- app/src/main/java/com/bradleywilcox/doodlepad/Drawing.java
+++ app/src/main/java/com/bradleywilcox/doodlepad/Drawing.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
@@ -15,6 +16,8 @@ import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 
+import java.util.Vector;
+
 /**
  * Bradley Wilcox / Michael Cha
  * CSCI 4020
@@ -22,6 +25,11 @@ import android.view.View;
  */
 
 public class Drawing extends View {
+
+    public enum Tools{
+        line,
+        rectangle
+    }
 
     private int currentWidth;
     private int currentHeight;
@@ -34,7 +42,9 @@ public class Drawing extends View {
     private float x_down, y_down;
 
     private RectF rect;
+    private Vector<Float> line;
 
+    private Tools tool;
 
     public Drawing(Context context) {
         super(context);
@@ -54,12 +64,21 @@ public class Drawing extends View {
     private void setup(AttributeSet attrs){
         paint = new Paint();
         paint.setColor(Color.RED);
-        paint.setStyle(Paint.Style.FILL);
+        paint.setStyle(Paint.Style.STROKE);
 
         rect = new RectF();
+        line = new Vector();
+        line.add(0, 1.0f);
+        line.add(1, 1.0f);
+        line.add(2, 1.0f);
+        line.add(3, 1.0f);
 
         DisplayMetrics dm = getResources().getDisplayMetrics();
         dpiPixel = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, dm);
+
+        paint.setStrokeWidth(dpiPixel);
+
+        setTool(Tools.line);
     }
 
     @Override
@@ -68,7 +87,11 @@ public class Drawing extends View {
 
 
         canvas.drawBitmap(bitmap, 0, 0, paint);
-        canvas.drawRect(rect, paint);
+
+        if(tool == Tools.rectangle)
+            canvas.drawRect(rect, paint);
+        else if(tool == Tools.line)
+            canvas.drawLine(line.get(0), line.get(1), line.get(2), line.get(3), paint);
     }
 
     @Override
@@ -78,28 +101,62 @@ public class Drawing extends View {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+
                 x_down = touchX;
                 y_down = touchY;
+                line.set(0, x_down);
+                line.set(1, y_down);
+                line.set(2, x_down);
+                line.set(3, y_down);
 
                 break;
+
             case MotionEvent.ACTION_MOVE:
 
-                //allow rectangle to be drawn in all directions
-                rect.set(x_down > touchX ? touchX : x_down,
-                         y_down > touchY ? touchY : y_down,
-                         touchX < x_down ? x_down : touchX,
-                         touchY < y_down ? y_down : touchY);
+                if(tool == Tools.rectangle)
+                    //allow rectangle to be drawn in all directions
+                    rect.set(x_down > touchX ? touchX : x_down,
+                             y_down > touchY ? touchY : y_down,
+                             touchX < x_down ? x_down : touchX,
+                             touchY < y_down ? y_down : touchY);
+
+                else if(tool == Tools.line) {
+                    line.set(2, touchX);
+                    line.set(3, touchY);
+                }
 
                 break;
+
             case MotionEvent.ACTION_UP:
-                drawingCanvas.drawRect(rect, paint);
+
+                if(tool == Tools.rectangle)
+                    drawingCanvas.drawRect(rect, paint);
+                else if(tool == Tools.line) {
+                    drawingCanvas.drawLine(line.get(0), line.get(1), line.get(2), line.get(3), paint);
+                    line.set(0, -1.0f);
+                    line.set(1, -1.0f);
+                    line.set(2, -1.0f);
+                    line.set(3, -1.0f);
+                }
+
                 break;
+
             default:
                 return false;
         }
 
         invalidate();
         return true;
+    }
+
+    public void setTool(Tools tool){
+        this.tool = tool;
+
+        if(this.tool == Tools.rectangle){
+            paint.setStyle(Paint.Style.FILL);
+        }else if(this.tool == Tools.line){
+            paint.setStyle(Paint.Style.STROKE);
+        }
     }
 
     @Override
--- app/src/main/java/com/bradleywilcox/doodlepad/MainActivity.java
+++ app/src/main/java/com/bradleywilcox/doodlepad/MainActivity.java
@@ -1,10 +1,15 @@
 package com.bradleywilcox.doodlepad;
 
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageButton btnLineTool, btnRectTool;
+    private Drawing drawingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,21 @@ public class MainActivity extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
 
         setContentView(R.layout.activity_main);
+
+        btnLineTool = (ImageButton) findViewById(R.id.btnLineTool);
+        btnRectTool = (ImageButton) findViewById(R.id.btnRectangleTool);
+        drawingView = (Drawing) findViewById(R.id.drawing_view);
+
+        btnLineTool.setOnClickListener(this);
+        btnRectTool.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == btnLineTool){
+            drawingView.setTool(Drawing.Tools.line);
+        }else if(view == btnRectTool){
+            drawingView.setTool(Drawing.Tools.rectangle);
+        }
     }
 }
